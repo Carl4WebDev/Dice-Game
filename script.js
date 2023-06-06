32fe0fa578880ae5326248abe07bef5c22e3a256
carl Ivan Sampan <carlforwebdev@gmail.com>
2023-06-06 08:43:02 +0600

    Bug fixed

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-let randomNumber1 = Math.floor((Math.random()*6) + 1) 
-let randomNumber2 = Math.floor((Math.random()*6) + 1) 
+let randomNumber1 = Math.floor(Math.random() * 7) 
+let randomNumber2 = Math.floor(Math.random() * 7) 
 
 let imageSource1 = 'images/dice' + randomNumber1 + '.png';
-document.querySelectorAll('img')[0].setAttribute('src', imageSource1)
+$('.img1').attr('src', imageSource1)
 
 
 let imageSource2 = 'images/dice' + randomNumber2 + '.png';
-document.querySelectorAll('img')[1].setAttribute('src', imageSource2)
+$('.img2').attr('src', imageSource2)
 
 
 if(randomNumber1 > randomNumber2){
-    document.querySelector('h1').textContent = "🚩 Play 1 Wins!";
+    $('h1').text("🚩 Play 1 Wins!")
 } else if(randomNumber2 > randomNumber1){
-    document.querySelector('h1').textContent = "🚩 Play 2 Wins!"
+    $('h1').text("🚩 Play 2 Wins!")
 } else {
-    document.querySelector('h1').textContent = "draw!"
+    $('h1').text("draw!")
+}
+
+if(randomNumber1 == 0){
+    $('.img1').addClass('zero')
+} else if(randomNumber2 == 0){
+    $('.img2').addClass('zero')
 }
 
 
